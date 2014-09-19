fa3259ae1a9fda16ff898ab7bcecc226fad11bcd
hilman <hilmanbeyri@gmail.com>
2014-09-19 17:53:27 -0400

    + playerhand

--- ProjectRound2/Assets/Scripts/GameController.cs
+++ ProjectRound2/Assets/Scripts/GameController.cs
@@ -6,11 +6,13 @@ public class GameController : MonoBehaviour {
 	public int canvasWidth;
 	public int canvasHeight;
 	public bool simulateWithMouse;
+	public int maxHands;
 
 	Texture2D canvasTexture;
 	GameObject canvasObject;
 	Color drawColor;
 	BrushShape brushShape;
+	PlayerHand[] hands;
 
 	void Awake()
 	{
@@ -18,42 +20,59 @@ public class GameController : MonoBehaviour {
 		canvasObject = GameObject.Find ("Canvas");
 		drawColor = Color.black;
 		brushShape = BrushShape.Circle;
+		hands = new PlayerHand[maxHands];
 	}
 
 	void Start()
 	{
 		canvasObject.renderer.material.mainTexture = canvasTexture;
 		InitCanvasTexture ();
+		InitPlayerHands ();
 	}
 
 	void Update()
 	{
-		Vector2 handCoord;
-
 		if (simulateWithMouse) 
 		{
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit hitInfo;
-			int layerMask = (1 << LayerMask.NameToLayer ("Canvas"));;
+			int layerMask = (1 << LayerMask.NameToLayer ("Canvas"));
 			Debug.DrawRay (ray.origin, ray.direction * 10, Color.yellow);
 
-			Physics.Raycast(ray, out hitInfo, 100, layerMask);
-			handCoord = hitInfo.textureCoord;
+			Physics.Raycast (ray, out hitInfo, 100, layerMask);
+			hands[0].prevPos = hands[0].pos;
+			//hands[0].pos = hitInfo.textureCoord;
+			hands[0].pos.x = (int) (hitInfo.textureCoord.x * canvasWidth - brushShape.width / 2.0f);
+			hands[0].pos.y = (int) (hitInfo.textureCoord.y * canvasHeight - brushShape.height / 2.0f);
 			//print (handCoord.x + " " + handCoord.y);
-			if(Input.GetMouseButton(0))
-				DrawBrush(handCoord, drawColor, brushShape);
+
+			hands [0].prevIsHandDown = hands [0].isHandDown;
+			hands [0].isHandDown = Input.GetMouseButton (0);
+		}
+		else
+		{
+			// TODO : kinect part
+		}
+
+		for (int i=0; i<maxHands; i++) 
+		{
+			if (hands [i].isHandDown) 
+			{
+				DrawBrush (hands[i].pos, drawColor, brushShape);
+				ConnectBrushPoint(hands[i]);
+			}
 		}
 	}
 
-	void DrawBrush(Vector2 uv, Color color, BrushShape brushShape)
+	void DrawBrush(Vector2 pos, Color color, BrushShape brushShape)
 	{
 		//ConvertUVToPixel (ref uv);
 		int k = 0;
 		int x, y;
-		y = (int) (uv.y * canvasHeight - brushShape.height / 2.0f);
+		y = (int) pos.y;
 		for (int i = brushShape.height - 1; i >= 0; i--) 
 		{
-			x = (int) (uv.x * canvasWidth - brushShape.width / 2.0f);
+			x = (int) pos.x;
 
 			for(int j = 0;j < brushShape.width; j++)
 			{
@@ -73,6 +92,15 @@ public class GameController : MonoBehaviour {
 		canvasTexture.Apply ();
 	}
 
+	void ConnectBrushPoint(PlayerHand hand)
+	{
+		if (!hand.prevIsHandDown || !hand.isHandDown)
+			return;
+
+		//connect current pos and prev pos
+		//DrawLine (canvasTexture, (int)hand.prevPos.x, (int)hand.prevPos.y, (int)hand.pos.x, (int)hand.pos.y, drawColor);
+	}
+
 	void InitCanvasTexture()
 	{
 		for (int i=0; i<canvasWidth; i++) 
@@ -84,5 +112,12 @@ public class GameController : MonoBehaviour {
 		}
 		canvasTexture.Apply ();
 	}
-	
+
+	void InitPlayerHands()
+	{
+		for (int i = 0; i<maxHands; i++) 
+		{
+			hands[i] = new PlayerHand();
+		}
+	}
 }
---
+++ ProjectRound2/Assets/Scripts/PlayerHand.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHand : MonoBehaviour {
+
+	public Vector2 pos = new Vector2();
+	public Vector2 prevPos = new Vector2();
+	public bool isHandDown;
+	public bool prevIsHandDown;
+
+
+}
