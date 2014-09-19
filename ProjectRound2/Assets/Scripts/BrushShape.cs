b463444518758e8e66f9fe834a8e794a95a92942
hilman <hilmanbeyri@gmail.com>
2014-09-18 21:05:36 -0400

    - initial commit for round 2 project

---
+++ ProjectRound2/Assets/Scripts/BrushShape.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrushShape : MonoBehaviour {
+
+	public byte[] matrix; // flattened 2d array
+	public static int width;
+	public static int height;
+
+	public BrushShape()
+	{
+
+	}
+
+}
---
+++ ProjectRound2/Assets/Scripts/GameController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public int canvasWidth;
+	public int canvasHeight;
+	public bool simulateWithMouse;
+
+	Texture2D canvasTexture;
+	GameObject canvasObject;
+	Color drawColor;
+
+	void Awake()
+	{
+		canvasTexture = new Texture2D (canvasWidth, canvasHeight, TextureFormat.RGBA32, false);
+		canvasObject = GameObject.Find ("Canvas");
+		drawColor = Color.black;
+	}
+
+	void Start()
+	{
+		canvasObject.renderer.material.mainTexture = canvasTexture;
+		InitCanvasTexture ();
+	}
+
+	void Update()
+	{
+		Vector2 handCoord;
+
+		if (simulateWithMouse) 
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hitInfo;
+			int layerMask = (1 << LayerMask.NameToLayer ("Canvas"));;
+			Debug.DrawRay (ray.origin, ray.direction * 10, Color.yellow);
+
+			Physics.Raycast(ray, out hitInfo, 100, layerMask);
+			handCoord = hitInfo.textureCoord;
+			//print (handCoord.x + " " + handCoord.y);
+			if(Input.GetMouseButton(0))
+				DrawBrush(handCoord, drawColor);
+		}
+	}
+
+	void DrawBrush(Vector2 uv, Color color)
+	{
+		//ConvertUVToPixel (ref uv);
+		canvasTexture.SetPixel ((int)(uv.x * canvasWidth), (int)(uv.y * canvasHeight), drawColor);
+		canvasTexture.Apply ();
+	}
+
+	void InitCanvasTexture()
+	{
+		for (int i=0; i<canvasWidth; i++) 
+		{
+			for(int j=0;j<canvasHeight;j++)
+			{
+				canvasTexture.SetPixel(i, j, Color.white);
+			}
+		}
+		canvasTexture.Apply ();
+	}
+
+	//void ConvertUVToPixel()
+}
