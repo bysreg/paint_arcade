6a7e0c332580cfb7639261bc7da6a313becb1f02
hilman <hilmanbeyri@gmail.com>
2014-09-20 12:49:31 -0400

    - new thick line algo

--- ProjectRound2/Assets/Scripts/GameController.cs
+++ ProjectRound2/Assets/Scripts/GameController.cs
@@ -20,8 +20,9 @@ public class GameController : MonoBehaviour {
 	{
 		canvasTexture = new Texture2D (canvasWidth, canvasHeight, TextureFormat.RGBA32, false);
 		canvasObject = GameObject.Find ("Canvas");
-		drawColor = Color.black;
-		brushShape = BrushShape.Circle;
+		drawColor = Color.blue;
+		drawColor.a = 0f;
+		brushShape = BrushShape.CreateSquare (5, 5);
 		hands = new PlayerHand[maxHands];
 	}
 
@@ -39,21 +40,23 @@ public class GameController : MonoBehaviour {
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit hitInfo;
 			int layerMask = (1 << LayerMask.NameToLayer ("Canvas"));
-			Debug.DrawRay (ray.origin, ray.direction * 10, Color.yellow);
+			Debug.DrawRay (ray.origin, ray.direction * 100, Color.yellow);
 
-			Physics.Raycast (ray, out hitInfo, 100, layerMask);
-			hands[0].prevPos = hands[0].pos;
-			//hands[0].pos = hitInfo.textureCoord;
-			hands[0].pos.x = (int) (hitInfo.textureCoord.x * canvasWidth - brushShape.width / 2.0f);
-			hands[0].pos.y = (int) (hitInfo.textureCoord.y * canvasHeight - brushShape.height / 2.0f);
-			//print (handCoord.x + " " + handCoord.y);
+			bool inCanvas = Physics.Raycast (ray, out hitInfo, 100, layerMask);
 
 			hands [0].prevIsHandDown = hands [0].isHandDown;
-			hands [0].isHandDown = Input.GetMouseButton (0);
+			hands[0].prevPos = hands[0].pos;
+			hands[0].pos.x = (int) (hitInfo.textureCoord.x * canvasWidth);
+			hands[0].pos.y = (int) (hitInfo.textureCoord.y * canvasHeight);
+			//print (hitInfo.textureCoord.x + " " + hitInfo.textureCoord.y);
+
+			if (inCanvas)
+				hands [0].isHandDown = Input.GetMouseButton (0);
+			else
+				hands [0].isHandDown = false;
 		}
 		else
 		{
-			// TODO : kinect part
 			PlayerHand handData = KinectRightHand.GetComponent<PlayerHand>();
 			if(handData.isHandDown) {
 				float x = KinectRightHand.transform.position.x;
@@ -62,7 +65,7 @@ public class GameController : MonoBehaviour {
 				float height = canvasObject.collider.bounds.size.y;
 				int px = (int) ((width*.5f+x)/width*canvasWidth);
 				int py = (int) ((height*.5f+y)/height*canvasHeight);
-				
+
 				DrawBrush (new Vector2(px, py), drawColor, brushShape);
 			}
 
@@ -80,13 +83,12 @@ public class GameController : MonoBehaviour {
 
 	void DrawBrush(Vector2 pos, Color color, BrushShape brushShape)
 	{
-		//ConvertUVToPixel (ref uv);
 		int k = 0;
 		int x, y;
-		y = (int) pos.y;
+		y = (int) (pos.y - brushShape.height/ 2.0f);
 		for (int i = brushShape.height - 1; i >= 0; i--) 
 		{
-			x = (int) pos.x;
+			x = (int) (pos.x - brushShape.width / 2.0f);
 
 			for(int j = 0;j < brushShape.width; j++)
 			{
@@ -108,11 +110,55 @@ public class GameController : MonoBehaviour {
 
 	void ConnectBrushPoint(PlayerHand hand)
 	{
+		int x0, y0, x1, y1;
+		x0 = (int) Mathf.Clamp (hand.prevPos.x, 0, canvasWidth - 1);
+		y0 = (int) Mathf.Clamp (hand.prevPos.y, 0, canvasHeight - 1);
+		x1 = (int) Mathf.Clamp (hand.pos.x, 0, canvasWidth - 1);
+		y1 = (int) Mathf.Clamp (hand.pos.y, 0, canvasHeight - 1);
+
 		if (!hand.prevIsHandDown || !hand.isHandDown)
 			return;
 
-		//connect current pos and prev pos
-		//DrawLine (canvasTexture, (int)hand.prevPos.x, (int)hand.prevPos.y, (int)hand.pos.x, (int)hand.pos.y, drawColor);
+		print (x0 + " " + y0 + " " + x1 + " " + y1 + " " + hand.pos);
+			 
+ 		//connect current pos and prev pos
+		DrawLine (hand.prevPos, hand.pos, drawColor);
+	}
+
+	void DrawLine(Vector2 pos0, Vector2 pos1, Color color)
+	{
+		float sx, sy, err, e2;
+		float dx = Mathf.Abs(pos1.x - pos0.x);
+		float dy = Mathf.Abs(pos1.y - pos0.y); 
+		if (pos0.x < pos1.x)
+			sx = 1; 
+		else 
+			sx = -1;
+		if (pos0.y < pos1.y)  
+			sy = 1; 
+		else 
+			sy = -1;
+		err = dx - dy;
+		
+		while(true)
+		{
+			DrawBrush(pos0, color, brushShape);
+			if (pos0.x == pos1.x && pos0.y == pos1.y) 
+				break;
+			e2 = 2 * err;
+			if (e2 > -dy) 
+			{
+				err = err - dy;
+				pos0.x = pos0.x + sx;
+			}
+			if (e2 < dx)
+			{
+				err = err + dx;
+				pos0.y = pos0.y + sy ;
+			}
+		}
+
+		canvasTexture.Apply ();
 	}
 
 	void InitCanvasTexture()
@@ -126,42 +172,12 @@ public class GameController : MonoBehaviour {
 		}
 		canvasTexture.Apply ();
 	}
-
+	
 	void InitPlayerHands()
 	{
 		for (int i = 0; i<maxHands; i++) 
 		{
 			hands[i] = new PlayerHand();
-		}
-	}
-
-//	void DrawLine(Vector2 pos0, Vector2 pos1, Color color)
-//	{
-//		float m = (pos1.y - pos0.y
-//	}
-
-	void plotLineWidth(int x0, int y0, int x1, int y1, float wd)
-   	{ 
-		int dx = Mathf.Abs(x1-x0), sx = x0 < x1 ? 1 : -1; 
-		int dy = Mathf.Abs(y1-y0), sy = y0 < y1 ? 1 : -1; 
-		int err = dx-dy, e2, x2, y2;                          /* error value e_xy */
-		float ed = dx+dy == 0 ? 1 : Mathf.Sqrt((float)dx*dx+(float)dy*dy);
-
-		for (wd = (wd+1)/2; ; ) {                                   /* pixel loop */
-			setPixelColor(x0, y0, Mathf.Max(0,255*(Mathf.Abs(err-dx+dy)/ed-wd+1));
-		    e2 = err; x2 = x0;
-		    if (2*e2 >= -dx) {                                           /* x step */
-				for (e2 += dy, y2 = y0; e2 < ed*wd && (y1 != y2 || dx > dy); e2 += dx)
-					canvasTexture.SetPixel(x0, y2 += sy, Mathf.Max(0,255*(Mathf.Abs(e2)/ed-wd+1));
-				if (x0 == x1) break;
-				e2 = err; err -= dy; x0 += sx; 
-			} 
-			if (2*e2 <= dy) {                                            /* y step */
-				for (e2 = dx-e2; e2 < ed*wd && (x1 != x2 || dx < dy); e2 += dy)
-					canvasTexture.SetPixel(x2 += sx, y0, Mathf.Max(0,255*(Mathf.Abs(e2)/ed-wd+1));
-				if (y0 == y1) break;
-				err += dx; y0 += sy; 
-			}
 		}
 	}
 }
