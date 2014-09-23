c1e578d70417d0e48dea3f20afbafe5f699f6a97
hilman <hilmanbeyri@gmail.com>
2014-09-22 22:39:59 -0400

    - transition scene script

---
+++ ProjectRound2/Assets/Scripts/Scenes/Level4Entry.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level4Entry : SceneEntry {
+
+	void Start () {
+		Screen.showCursor = false; //FIXME :
+		SoundManager.instance.PlayBGMusic (2);
+		
+	}
+
+	public override void ProcessDoneButton() {
+		ColorCapture2D[] colorCapture2Ds = GameObject.Find ("GameController").GetComponents<ColorCapture2D>();
+		for (int i=0; i<colorCapture2Ds.Length; i++) {
+			colorCapture2Ds[i].Impose();
+			colorCapture2Ds[i].TargetObj.SetActive(true);
+		}
+		
+	}
+}
