5271b453ba9227480cf475d3f77018794c15e1d4
hilman <hilmanbeyri@gmail.com>
2014-09-22 22:25:54 -0400

    - volatile

--- ProjectRound2/Assets/FishaMove.cs
+++ ProjectRound2/Assets/FishaMove.cs
@@ -1,22 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FishMove : MonoBehaviour {
+public class FishaMove : MonoBehaviour {
 
 	public float speed;
 	public int mode = 0;
-
+	
 	private float timeToRotate = 1f;
 	private float timer = 0;
 
-	void Update()
-	{
+	void Update () {
 		if (mode == 0) {
 			transform.Rotate (0, 0, -1, Space.World);
-			transform.position += -transform.up * speed * Time.deltaTime;
+			transform.position -= transform.right * speed * Time.deltaTime;
 		} else if (mode == 1) {
-			transform.position += -transform.up * speed * Time.deltaTime;
+			transform.position += transform.right * speed * Time.deltaTime;
 		}
 	}
-
 }
