576c65a5213cd7eae368fbfaddecca5e4dcfd483
hilman <hilmanbeyri@gmail.com>
2014-09-25 20:09:20 -0400

    - early implementation of the final scene

---
+++ ProjectRound2/Assets/Scripts/SpawnAnimations.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnAnimations : MonoBehaviour {
+
+	public Transform bird; // transform component required
+
+	void Start()
+	{
+		List<PlayerCreation> creations = GameController.GetSavedPlayerCreation ();
+
+		if (creations == null)
+			return;
+
+		for (int i=0; i<creations.Count; i++) 
+		{
+			if(creations[i].type == PlayerCreation.CreationType.Bird)
+			{
+				Instantiate(bird);
+				bird.position = Vector3.zero;
+			}
+		}
+
+
+	}
+
+}
