9b71ccd1fa72755e783d4aabb10c3a86b21d1bc3
hilman <hilmanbeyri@gmail.com>
2014-09-28 20:43:57 -0400

    - Credit scene ?

---
+++ ProjectRound2/Assets/Scripts/Scenes/FinalEntry.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Kinect;
+
+public class FinalEntry : SceneEntry {
+	public void Awake () {
+		SceneManager.instance.NextSceneName = "CreditScene";
+		//SoundManager.instance.PlayBGMusic (4);
+		Invoke ("LoadNext", 10f);
+
+	}
+	
+	public override void ProcessDoneButton() {
+	}
+	
+	void LoadNext() {
+		SceneManager.instance.asyncLoadNextSceneWithDelay (1f);
+	}
+}
